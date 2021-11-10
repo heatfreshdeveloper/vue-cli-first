7373b8853fe80f78d6bab0cd552a3390eb8f73f4
heatfreshdeveloper <72042291+heatfreshdeveloper@users.noreply.github.com>
2021-11-10 17:51:41 +0100

    Creato progetto e inserito il component Title, aggiunta image Hero

---
+++ src/components/Title.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="title">
+      <h1> {{mex}} </h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Title',
+    props: {
+        mex: String
+    }
+}
+</script>
+
+<style>
+
+</style>
